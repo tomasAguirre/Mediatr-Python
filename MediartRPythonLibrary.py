00a6d6b053c02a5ac8928de11cd67130fa97d282
DEVELOPERPC\jonat <jonathan3aguirre@outllook.com>
2025-10-14 20:52:23 -0600

    Primer commit: subiendo archivo de ejemplo de Meadiart

---
+++ MediartRPythonLibrary.py
@@ -0,0 +1,42 @@
+#pip install mediatr
+import asyncio
+from mediatr import Mediator
+
+
+class GetArrayQuery():
+    def __init__(self,items_count:int):
+        self.items_count = items_count
+
+class SumNumsQuery():
+    def __init__(self,num1:int, num2:int):
+        self.num1 = num1
+        self.num2 = num2
+
+@Mediator.handler
+class GetArrayQueryHandler():
+    def handle(self,request:GetArrayQuery):
+        items = list()
+        for i in range(0, request.items_count):
+            items.append(i)
+        return items
+    
+@Mediator.handler
+class SumNumsQueryHandler():
+    def handle(self,request:SumNumsQuery):
+        return request.num1 + request.num2
+    
+# or just Mediator.register_handler(get_array_handler)
+
+mediator = Mediator()
+
+async def main():
+    request = GetArrayQuery(5)
+    result = await mediator.send_async(request)
+    print(result)
+
+    request2 = SumNumsQuery(2,2)
+    result = await mediator.send_async(request2)
+
+    print("resultado suma 2 nums ", result)
+
+asyncio.run(main())
